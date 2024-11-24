94f6b9825319b2e711f89b98e94dbdf8775b2b3b
Duc Huy <chuahetheky@gmail.com>
2024-11-24 22:20:01 +0700

    [fe] course list page

--- frontend/components/Courses/CourseList.tsx
+++ frontend/components/Courses/CourseList.tsx
@@ -1,64 +1,68 @@
 'use client'
 
 import Link from 'next/link'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useToastContext } from '../Toast/ToastContext'
+
+type CourseType = {
+    _id: string; 
+    title: string;
+    description: string;
+    price: string; 
+    thumbnail: string; 
+    lessons: string[]; 
+    createdAt: string; 
+    updatedAt: string; 
+}
 
 const CourseList = () => {
 
-    // useEffect(() => {
-    //     fetch(`${process.env.NEXT_PUBLIC_BACKEND_LINK}/`)
-    // }, [])
+    const toast = useToastContext();
+    const [courses, setCourses] = useState<CourseType[] | null>(null);
+
+    useEffect(() => {
+        fetch(`${process.env.NEXT_PUBLIC_BACKEND_LINK}/course`, {
+            method: 'GET',
+            credentials: 'include'
+        })
+            .then(res => {
+                if (!res.ok) {
+                    res.json()
+                        .then((errorData: { error: string, message: string }) => {
+                            throw new Error(errorData.message || 'An error occured');
+                        })
+                        .catch(err => toast.addToast('error', err.message))
+                }
+                return res.json();
+            })
+            .then(data => setCourses(data))
+            .catch((err: Error) => toast.addToast('error', err.message))
+    }, [])
+
 
     return (
         <>
             <h1 className='text-3xl font-bold mb-4 ms-4'>Courses list</h1>
             <div className="grid grid-cols-2 md:grid-cols-3 gap-6">
-
-                <div className="max-w bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <Link href="#">
-                        <img className="rounded-t-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="" />
-                    </Link>
-                    <div className="p-5">
-                        <Link href="#">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                        </Link>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                    </div>
-                </div>
-                <div className="max-w bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <Link href="#">
-                        <img className="rounded-t-lg" src="/images/hero_background.jpg" alt="" />
-                    </Link>
-                    <div className="p-5">
-                        <Link href="#">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                        </Link>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                    </div>
-                </div>
-                <div className="max-w bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <Link href="#">
-                        <img className="rounded-t-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="" />
-                    </Link>
-                    <div className="p-5">
-                        <Link href="#">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                        </Link>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                    </div>
-                </div>
-                <div className="max-w bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <Link href="#">
-                        <img className="rounded-t-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="" />
-                    </Link>
-                    <div className="p-5">
-                        <Link href="#">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                        </Link>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                    </div>
-                </div>
-
+                {
+                    courses && courses.map(course => (
+                        <div key={course._id} className="max-w bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            <Link href={`/courses/${course._id}`}>
+                                <img className="rounded-t-lg" src={course.thumbnail} alt="course_thumbnail" />
+                            </Link>
+                            <div className="p-5">
+                                <Link href={`/courses/${course._id}`}>
+                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                        {course.title}
+                                    </h5>
+                                </Link>
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                    {course.description}
+                                </p>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
 
         </>
