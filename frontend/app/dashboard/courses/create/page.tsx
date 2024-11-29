d85c2d58ca54b7e5b6a6f9c630673ddb43a1f1eb
Duc Huy <chuahetheky@gmail.com>
2024-11-29 18:07:28 +0700

    [fe] create course form

---
+++ frontend/app/dashboard/courses/create/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CourseCreate from '@/components/Courses/CourseCreate'
+
+const CreateCoursePage = () => {
+    return (
+        <>
+          <CourseCreate />  
+        </>
+    )
+}
+
+export default CreateCoursePage
