a525f106c36fc5e15a75aff5a62e019d38d2652a
Duc Huy <chuahetheky@gmail.com>
2024-12-26 16:46:12 +0700

    [be] add course's lesson

--- backend/controllers/lessonController.js
+++ backend/controllers/lessonController.js
@@ -1,8 +1,9 @@
 const courseData = require('../models/courseModel')
+const lessonData = require('../models/lessonModel')
 
 class lessonController {
     // [GET] /course/:id/lesson
-    showLessons(req, res) {
+    index(req, res) {
         if (req.params && req.params.id) {
             courseData.findById(req.params.id).populate('lessons')
                 .then(course => res.status(200).json(course.lessons))
@@ -14,12 +15,12 @@ class lessonController {
     }
 
     // [GET] /course/:id/lesson/:order
-    showOneLesson(req, res) {
+    show(req, res) {
         if (req.params && req.params.id) {
             courseData.findById(req.params.id).populate('lessons')
                 .then(course => {
                     const lesson = course.lessons.find(ls => ls.order == req.params.order);
-                    if (!lesson || lesson === null) throw new Error(); 
+                    if (!lesson || lesson === null) throw new Error();
                     else return res.status(200).json(lesson);
                 })
                 .catch(() => res.status(400).json({
@@ -27,6 +28,63 @@ class lessonController {
                     message: 'Cannot find requested course'
                 }))
         }
+    }
+
+    // [POST] /courses/:id/lesson
+    store(req, res) {
+        const { title, description, thumbnail, video_url, content, order } = req.body;
+        const thumbnailPlacehoder = '/images/placeholder_image.png'
+
+        if (!title) {
+            return res.status(400).json({
+                error: 'invalid_input',
+                message: 'Title are required'
+            })
+        }
+
+        const newLesson = {
+            title,
+            description: description || '...',
+            thumbnail: thumbnail || thumbnailPlacehoder,
+            video_url: video_url || 'free',
+            content,
+            course: req.params.id,
+            order
+        }
+
+        // find course including the created lesson 
+        courseData.findById(req.params.id)
+            .then(course => {
+                // create lesson 
+                lessonData.create(newLesson)
+                    .then(createdLesson => {
+                        // update lessons field of the course 
+                        course.lessons.push(createdLesson._id); 
+                        course.save()
+                            .then(() => res.status(201).json(createdLesson))
+                            .catch(err => {
+                                console.log(err); 
+                                res.status(500).json({
+                                    error: 'course_not_updated',
+                                    message: 'Cannot update the course with new lesson'
+                                });
+                            })
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        res.status(500).json({
+                            error: 'lesson_not_created',
+                            message: 'An error occurred while creating the lesson'
+                        });
+                    })
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(404).json({
+                    error: 'lesson_create_error',
+                    message: `${err}`
+                });
+            })
     }
 
 }
